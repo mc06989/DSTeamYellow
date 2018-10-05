1b04ac3e4c7cf25a73586933101607826579a839
mc06989 <mc06989@georgiasouthern.edu>
2018-10-05 12:17:16 -0400

    Fixed App.java not building

--- dsteamyellow/src/main/java/edu/georgiasouthern/dsteamyellow/OrderPanel.java
+++ dsteamyellow/src/main/java/edu/georgiasouthern/dsteamyellow/OrderPanel.java
@@ -10,9 +10,9 @@ public class OrderPanel extends JPanel {
 		setLayout(springLayout);
 		
 		JPanel panel = new JPanel();
-		springLayout.putConstraint(SpringLayout.NORTH, panel, 10, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.NORTH, panel, -74, SpringLayout.SOUTH, this);
 		springLayout.putConstraint(SpringLayout.WEST, panel, 10, SpringLayout.WEST, this);
-		springLayout.putConstraint(SpringLayout.SOUTH, panel, -10, SpringLayout.SOUTH, this);
+		springLayout.putConstraint(SpringLayout.SOUTH, panel, -188, SpringLayout.SOUTH, this);
 		springLayout.putConstraint(SpringLayout.EAST, panel, 564, SpringLayout.WEST, this);
 		add(panel);
 
