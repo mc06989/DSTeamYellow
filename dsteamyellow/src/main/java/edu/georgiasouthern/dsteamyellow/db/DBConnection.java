3a5d5cd2f82937ffe39be131c547c70e886f7ca3
mc06989 <mc06989@CEIT2553214X003.ad.georgiasouthern.edu>
2018-10-16 16:18:21 -0400

    ORMLite now makes a connection to the live db
    - Fixed DBConnection.java so it properly connects
    - Added Category to NorthwindTableDefinitions.java

--- dsteamyellow/src/main/java/edu/georgiasouthern/dsteamyellow/db/DBConnection.java
+++ dsteamyellow/src/main/java/edu/georgiasouthern/dsteamyellow/db/DBConnection.java
@@ -2,9 +2,11 @@ package edu.georgiasouthern.dsteamyellow.db;
 
 import java.sql.SQLException;
 
+import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.jdbc.JdbcConnectionSource;
+import com.j256.ormlite.spring.DaoFactory;
 import com.j256.ormlite.support.ConnectionSource;
-
+import edu.georgiasouthern.dsteamyellow.db.NorthwindTableDefinitions.*;
 public class DBConnection {
 	
 	private static volatile DBConnection sdbconnection;
@@ -20,7 +22,16 @@ public class DBConnection {
 	private DBConnection() {
 		String databaseUrl = "jdbc:sqlserver://dbyellowteam.database.windows.net:1433;database=dbyellowteam;user=dbyellow@dbyellowteam;password=Dbyell0wteam;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;";
 		ConnectionSource connectionSource = new JdbcConnectionSource(databaseUrl);
-		
+		System.out.println(connectionSource.toString());
+		try {
+			new DaoFactory();
+			Dao<Category, Integer> dao = DaoFactory.createDao(connectionSource, Category.class);
+			Category c = dao.queryForId(2);
+			System.out.println(c.getCategoryName());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 }
--- dsteamyellow/src/main/java/edu/georgiasouthern/dsteamyellow/db/NorthwindTableDefinitions.java
+++ dsteamyellow/src/main/java/edu/georgiasouthern/dsteamyellow/db/NorthwindTableDefinitions.java
@@ -1,5 +1,42 @@
 package edu.georgiasouthern.dsteamyellow.db;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
 public class NorthwindTableDefinitions {
 
+	@DatabaseTable(tableName="categories")
+	static class Category{
+		@DatabaseField(id=true)
+		private int CategoryID;
+		@DatabaseField(canBeNull=false)
+		private String CategoryName;
+		@DatabaseField(canBeNull=true)
+		private String Description;
+		
+		public Category() {
+			
+		}
+		
+		public int getCategoryID() {
+			return CategoryID;
+		}
+		public void setCategoryID(int categoryID) {
+			CategoryID = categoryID;
+		}
+		public String getCategoryName() {
+			return CategoryName;
+		}
+		public void setCategoryName(String categoryName) {
+			CategoryName = categoryName;
+		}
+		public String getDescription() {
+			return Description;
+		}
+		public void setDescription(String description) {
+			Description = description;
+		}
+		
+	}
+	
 }
